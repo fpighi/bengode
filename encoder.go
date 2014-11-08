e51de955e418cd6d1f13fab82d8857bdab1e70e2
Francesco Pighi <francesco.pighi@gmail.com>
2014-11-08 10:59:25 +0000

    encode list and all int types

--- encoder.go
+++ encoder.go
@@ -2,7 +2,9 @@ package encoder
 
 import (
 	"bytes"
+	"container/list"
 	"fmt"
+	"reflect"
 )
 
 func Marshal(v interface{}) (string, error) {
@@ -16,11 +18,9 @@ type encodeProcess struct {
 }
 
 func (e *encodeProcess) marshal(v interface{}) (err error) {
-	s, isString := v.(string)
-	if isString {
-		encoded := stringEncodeFunc()(s)
-		e.b.WriteString(encoded)
-	}
+	encoded, _ := getEncodedValue(v)
+
+	e.b.WriteString(encoded)
 	return nil
 }
 
@@ -28,11 +28,53 @@ func (e *encodeProcess) getString() string {
 	return e.b.String()
 }
 
-func stringEncodeFunc() func(string) string {
-	return func(s string) string {
-		if len(s) == 0 {
-			return ""
-		}
-		return fmt.Sprint(len(s), ":", s)
+func getEncodedValue(v interface{}) (string, *TypeNotSupportedError) {
+	switch x := v.(type) {
+	case int:
+		return encodeInt64(int64(x)), nil
+	case int8:
+		return encodeInt64(int64(x)), nil
+	case int16:
+		return encodeInt64(int64(x)), nil
+	case int32:
+		return encodeInt64(int64(x)), nil
+	case int64:
+		return encodeInt64(int64(x)), nil
+	case string:
+		return encodeString(string(x)), nil
+	case *list.List:
+		return encodeList(x), nil
+	default:
+		fmt.Printf("The type is %v", reflect.TypeOf(v))
+		return "", &TypeNotSupportedError{msg: "Type not supported", Type: reflect.TypeOf(v).String()}
 	}
 }
+
+func encodeList(l *list.List) string {
+	var buffer bytes.Buffer
+	buffer.WriteString("l")
+	for e := l.Front(); e != nil; e = e.Next() {
+		encoded, _ := getEncodedValue(e.Value)
+		buffer.WriteString(encoded)
+	}
+	buffer.WriteString("e")
+	return buffer.String()
+}
+
+func encodeString(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+	return fmt.Sprint(len(s), ":", s)
+}
+
+func encodeInt64(n int64) string {
+	return fmt.Sprintf("i%de", n)
+}
+
+type TypeNotSupportedError struct {
+	msg  string
+	Type string
+}
+
+func (e *TypeNotSupportedError) Error() string { return e.msg }
--- encoder_test.go
+++ encoder_test.go
@@ -1,6 +1,7 @@
 package encoder
 
 import (
+	"container/list"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -13,8 +14,65 @@ func TestEncodingToString(t *testing.T) {
 }
 
 func TestEncodingEmptyString(t *testing.T) {
-	bencodeString, err := Marshal("")
+	bencodedString, err := Marshal("")
 
-	assert.Equal(t, "", bencodeString)
+	assert.Equal(t, "", bencodedString)
 	assert.Nil(t, err)
+}
+
+func TestEncodingInt8(t *testing.T) {
+	v := int8(123)
+	bencodedInt, err := Marshal(v)
+
+	assert.Equal(t, "i123e", bencodedInt)
+	assert.Nil(t, err)
+}
+
+func TestEncodingInt16(t *testing.T) {
+	v := int16(123)
+	bencodedInt, err := Marshal(v)
+
+	assert.Equal(t, "i123e", bencodedInt)
+	assert.Nil(t, err)
+}
+
+func TestEncodingInt32(t *testing.T) {
+	v := int32(123)
+	bencodedInt, err := Marshal(v)
+
+	assert.Equal(t, "i123e", bencodedInt)
+	assert.Nil(t, err)
+}
+
+func TestEncodingInt64(t *testing.T) {
+	v := int64(123)
+	bencodedInt, err := Marshal(v)
+
+	assert.Equal(t, "i123e", bencodedInt)
+	assert.Nil(t, err)
+}
+
+func TestEncodingNegativeNumber(t *testing.T) {
+	v := -42
+	bencodedInt, err := Marshal(v)
+
+	assert.Equal(t, "i-42e", bencodedInt)
+	assert.Nil(t, err)
+}
+
+func TestEncodingList(t *testing.T) {
+	l := list.New()
+	l.PushBack("pippo")
+	l.PushBack(123)
+
+	bencodedList, err := Marshal(l)
+
+	assert.Equal(t, "l5:pippoi123ee", bencodedList)
+	assert.Nil(t, err)
+}
+
+func BenchmarkEncodingToString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Marshal("pippo")
+	}
 }
